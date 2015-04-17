6bad920636cfe572c9414566a740cb870a943f7d
moof mayeda <moofmayeda@yahoo.com>
2015-04-17 15:29:32 -0700

    Initial set up

--- js/app.js
+++ js/app.js
@@ -1,3 +1,36 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+$(document).ready(function() {
+  function filterPath(string) {
+    return string
+      .replace(/^\//,'')
+      .replace(/(index|default).[a-zA-Z]{3,4}$/,'')
+      .replace(/\/$/,'');
+  }
+  $('a[href*=#]').each(function() {
+    if ( filterPath(location.pathname) == filterPath(this.pathname)
+    && location.hostname == this.hostname
+    && this.hash.replace(/#/,'') ) {
+      var $targetId = $(this.hash), $targetAnchor = $('[name=' + this.hash.slice(1) +']');
+      var $target = $targetId.length ? $targetId : $targetAnchor.length ? $targetAnchor : false;
+       if ($target) {
+         var targetOffset = $target.offset().top - 50;
+         $(this).click(function() {
+           $('html, body').animate({scrollTop: targetOffset}, 750);
+           var d = document.createElement("div");
+        d.style.height = "101%";
+        d.style.overflow = "hidden";
+        document.body.appendChild(d);
+        setTimeout(function() {
+        d.parentNode.removeChild(d);
+            }, 10);
+           return false;
+         });
+      }
+    }
+  });
+});
+// Smooth Scroll - v1.4.5 - 2012-07-22
+// Copyright (c) 2012 Karl Swedberg; Licensed MIT, GPL
