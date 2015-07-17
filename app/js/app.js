1d99367aaefd34700561828ff3667742a519d0ff
moof mayeda <moofmayeda@yahoo.com>
2015-07-17 10:07:07 -0700

    Add yeoman and grunt

---
+++ app/js/app.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var app = (function(document, $) {
+	var docElem = document.documentElement,
+		_userAgentInit = function() {
+			docElem.setAttribute('data-useragent', navigator.userAgent);
+		},
+		_init = function() {
+			$(document).foundation();
+            // needed to use joyride
+            // doc: http://foundation.zurb.com/docs/components/joyride.html
+            $(document).on('click', '#start-jr', function () {
+                $(document).foundation('joyride', 'start');
+            });
+			_userAgentInit();
+		};
+	return {
+		init: _init
+	};
+})(document, jQuery);
+
+(function() {
+	app.init();
+})();
+
+$(document).ready(function() {
+  $('nav.top-bar a').smoothScroll({offset: -44, speed: 500});
+});
